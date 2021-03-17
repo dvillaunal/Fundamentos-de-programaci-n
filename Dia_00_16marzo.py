0d9bcade49af9b0dbb6a383c4d0b78f921cf5124
dvillaunal <56985754+dvillaunal@users.noreply.github.com>
2021-03-17 01:09:39 -0500

    Create Dia_00_16marzo.py

---
+++ Dia_00_16marzo.py
@@ -0,0 +1,8 @@
+# Funciones Predeterminadas:
+'''
+una de ellas es la función print()
+la cual al ingresarle un valor o varios
+imprime...
+mostrando el resultado en pantalla como lo veremos en el siguiente ejemplo:
+'''
+print("Hola mundo")
